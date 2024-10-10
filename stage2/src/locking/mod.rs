68a8204d559a85beca2bf7b05e8c215815b6b4e7
Tom Dohrmann <erbse.13@gmx.de>
2024-10-10 06:31:33 +0000

    stage2: remove IrqUnsafeLocking from SpinLock
    We only ever use irq unsafe locking, so we don't need the extra generic
    parameter.
    
    Signed-off-by: Tom Dohrmann <erbse.13@gmx.de>

--- stage2/src/locking/mod.rs
+++ stage2/src/locking/mod.rs
@@ -4,12 +4,8 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
-pub mod common;
 pub mod rwlock;
 pub mod spinlock;
 
-pub use common::{IrqLocking, IrqSafeLocking, IrqUnsafeLocking};
-pub use rwlock::{
-    RWLock, ReadLockGuard, ReadLockGuardIrqSafe, WriteLockGuard, WriteLockGuardIrqSafe,
-};
-pub use spinlock::{LockGuard, LockGuardIrqSafe, RawLockGuard, SpinLock};
+pub use rwlock::{RWLock, ReadLockGuard, WriteLockGuard};
+pub use spinlock::{LockGuard, SpinLock};
