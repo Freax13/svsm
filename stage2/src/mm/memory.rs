af3123337492efa9e3b401b1744512ed406774e5
Tom Dohrmann <erbse.13@gmx.de>
2024-11-05 09:20:55 +0000

    stage2: remove init_memory_map and write_guest_memory_map

--- stage2/src/mm/memory.rs
+++ stage2/src/mm/memory.rs
@@ -7,94 +7,15 @@
 extern crate alloc;
 
 use crate::address::{Address, PhysAddr};
-use crate::config::SvsmConfig;
 use crate::cpu::percpu::PERCPU_VMSAS;
-use crate::error::SvsmError;
 use crate::locking::RWLock;
 use crate::utils::MemoryRegion;
 use alloc::vec::Vec;
-use bootlib::kernel_launch::KernelLaunchInfo;
 
 use super::pagetable::LAUNCH_VMSA_ADDR;
 
 /// Global memory map containing various memory regions.
 static MEMORY_MAP: RWLock<Vec<MemoryRegion<PhysAddr>>> = RWLock::new(Vec::new());
-
-/// Initializes the global memory map based on the provided configuration
-/// and kernel launch information.
-///
-/// # Arguments
-///
-/// * `config` - A reference to the [`SvsmConfig`] containing memory region
-///   information.
-/// * `launch_info` - A reference to the [`KernelLaunchInfo`] containing
-///   information about the kernel region.
-///
-/// # Returns
-///
-/// Returns `Ok(())` if the memory map is successfully initialized, otherwise
-/// returns an error of type `SvsmError`.
-pub fn init_memory_map(
-    config: &SvsmConfig<'_>,
-    launch_info: &KernelLaunchInfo,
-) -> Result<(), SvsmError> {
-    let mut regions = config.get_memory_regions()?;
-    let kernel_start = PhysAddr::from(launch_info.kernel_region_phys_start);
-    let kernel_end = PhysAddr::from(launch_info.kernel_region_phys_end);
-    let kernel_region = MemoryRegion::from_addresses(kernel_start, kernel_end);
-
-    // Remove SVSM memory from guest memory map
-    let mut i = 0;
-    while i < regions.len() {
-        // Check if the region overlaps with SVSM memory.
-        let region = regions[i];
-        if !region.overlap(&kernel_region) {
-            // Check the next region.
-            i += 1;
-            continue;
-        }
-
-        // 1. Remove the region.
-        regions.remove(i);
-
-        // 2. Insert a region up until the start of SVSM memory (if non-empty).
-        let region_before_start = region.start();
-        let region_before_end = kernel_region.start();
-        if region_before_start < region_before_end {
-            regions.insert(
-                i,
-                MemoryRegion::from_addresses(region_before_start, region_before_end),
-            );
-            i += 1;
-        }
-
-        // 3. Insert a region up after the end of SVSM memory (if non-empty).
-        let region_after_start = kernel_region.end();
-        let region_after_end = region.end();
-        if region_after_start < region_after_end {
-            regions.insert(
-                i,
-                MemoryRegion::from_addresses(region_after_start, region_after_end),
-            );
-            i += 1;
-        }
-    }
-
-    log::info!("Guest Memory Regions:");
-    for r in regions.iter() {
-        log::info!("  {:018x}-{:018x}", r.start(), r.end());
-    }
-
-    let mut map = MEMORY_MAP.lock_write();
-    *map = regions;
-
-    Ok(())
-}
-
-pub fn write_guest_memory_map(config: &SvsmConfig<'_>) -> Result<(), SvsmError> {
-    // Supply the memory map to the guest if required by the configuration.
-    config.write_guest_memory_map(&MEMORY_MAP.lock_read())
-}
 
 /// Returns `true` if the provided physical address `paddr` is valid, i.e.
 /// it is within the configured memory regions, otherwise returns `false`.
